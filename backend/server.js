0a53e1f0fe818d9fe08b198f8c59e8eae87d89d7
Dan Mogaka <dannixon360@gmail.com>
2026-05-26 16:33:30 -0500

    Backend server setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
