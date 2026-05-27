743372822a3a0175de5c6fae7601cad01a9f9aa1
Dan Mogaka <dannixon360@gmail.com>
2026-05-26 19:02:10 -0500

    Backend server set up and defined auth routes

---
+++ backend/routes/auth.js
@@ -0,0 +1,104 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import pool from "../config/db.js";
+import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // Only send cookies over https during production
+  sameSite: "Strict", // prevent crsf attacks
+  maxAge: 15 * 60 * 1000, // Cookies expires after 15 mins
+};
+
+// signs user using their id
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+};
+
+router.post("/register", async (req, res) => {
+  const { name, username, email, password } = req.body;
+
+  if (!username || !name || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide all required fields" });
+  }
+
+  // Already user with current email in the databse error handling
+  const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+
+  if (userExists.rows.length > 0) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10); // 10 salt rounds
+
+  const newUser = await pool.query(
+    "INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING id, name, username, email",
+    [name, username, email, hashedPassword],
+  );
+
+  /// Generating and storing tokens in the response to the client's browser
+  const token = generateToken(newUser.rows[0].id);
+
+  res.cookie("token", token, cookieOptions);
+
+  return res.status(201).json({ user: newUser.rows[0] });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide all required fields" });
+  }
+
+  const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+
+  if (user.rows.length === 0) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const userData = user.rows[0];
+
+  const isMatch = await bcrypt.compare(password, userData.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = generateToken(userData.id);
+
+  res.cookie("token", token, cookieOptions);
+
+  res.json({
+    user: {
+      id: userData.id,
+      name: userData.name,
+      username: userData.username,
+      email: userData.email,
+    },
+  });
+});
+
+// Route for logged in user from the protect middleware
+router.get("/me", protect, async (req, res) => {
+  res.json(req.user);
+});
+
+router.post("/logout", (req, res) => {
+  res.cookie("token", "", { ...cookieOptions, maxAge: 1 });
+  res.json({ message: "Logged out successfuly" });
+});
+
+export default router;
