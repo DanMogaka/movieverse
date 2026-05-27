d4b1d42bd81b65b695064c99165184e25ffe917c
Dan Mogaka <dannixon360@gmail.com>
2026-05-26 22:54:25 -0500

    Prepared database config for production

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,19 +3,30 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      },
+);
 
 pool.on("connect", () => {
-  console.log("Connected to the databse");
+  console.log("Connected to the database");
 });
 
-pool.on("eror", () => {
+pool.on("error", () => {
   console.error("Unexpected error on idle client", err);
 });
 
